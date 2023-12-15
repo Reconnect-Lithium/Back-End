44db8f727ea4ff4601e0d0d7dc1bd1c78f2b7e88
NEIR-0 <aalijlal.ganteng@gmail.com>
2023-12-15 13:50:52 +0700

    feat: create message

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,4 +1,12 @@
-const { User, Gallery, Occasion, Cafe, sequelize, Room } = require("../models");
+const {
+  User,
+  Gallery,
+  Occasion,
+  Cafe,
+  sequelize,
+  Room,
+  Message,
+} = require("../models");
 
 class roomControllers {
   static async createEvent(req, res, next) {
@@ -8,7 +16,8 @@ class roomControllers {
         req.body;
       const cafeId = await Cafe.findOne({ UserId: userId });
 
-      await Occasion.create({
+      // create event
+      const eventId = await Occasion.create({
         startTime,
         endTime,
         description,
@@ -18,9 +27,39 @@ class roomControllers {
         CafeId: cafeId.id,
       });
 
+      // create roooms
+      await Room.create({
+        OccasionId: eventId.id,
+        UserId: userId,
+      });
+
       res.send({ message: `success add ${eventName} as new event` });
     } catch (error) {
       console.log(error);
+      next(error);
+    }
+  }
+
+  //   joinRoom
+  static async joinRoom(req, res, next) {
+    try {
+      const userId = req.user.id;
+      const { roomId } = req.params;
+      const { message } = req.body;
+      //   console.log(roomId, userId, message);
+
+      const newMessage = await Message.create({
+        RoomId: roomId,
+        UserId: userId,
+        message: message,
+        time: Date.now(),
+      });
+      //   console.log(message);
+
+      res.send(newMessage);
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
   }
 }
--- routers/room.js
+++ routers/room.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const roomControllers = require("../controllers/roomControllers");
 
+router.post("/:roomId", roomControllers.joinRoom);
 router.post("/create-event", roomControllers.createEvent);
-// router.get("/create-room", roomControllers.createRoom);
 
 module.exports = router;
