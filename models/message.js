717bab914303c83c6b090f7e2d85a148d7d5f1d6
NEIR-0 <aalijlal.ganteng@gmail.com>
2023-12-18 18:24:26 +0700

    fix: bug-1

--- models/message.js
+++ models/message.js
@@ -39,7 +39,7 @@ module.exports = (sequelize, DataTypes) => {
           },
         },
       },
-      RoomId: DataTypes.NUMBER,
+      RoomId: DataTypes.STRING,
       UserId: DataTypes.NUMBER,
     },
     {
